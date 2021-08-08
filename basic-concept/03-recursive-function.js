08c6a06a34f76db522b300a22c8ce08141254f36
imseongtae <dev.haemil@gmail.com>
2021-08-09 08:34:09 +0900

    feat: Update 03-recursive-function to include solution using loop

--- basic-concept/03-recursive-function.js
+++ basic-concept/03-recursive-function.js
@@ -1,5 +1,6 @@
 // 2진수 변환
 
+// recursive
 let result = '';
 function rf(num) {
   if (num % 2 === 0) {
@@ -20,6 +21,25 @@ function rf2(num) {
   return rf2(Math.floor(num / 2)) + String(num % 2);
 }
 
+// loop
+function lf(num) {
+  let result = '';
+  let isFlag = true;
+  while (isFlag) {
+    if (num % 2 === 0) {
+      result = '0' + result;
+    } else {
+      result = '1' + result;
+    }
+    num = Math.floor(num / 2);
+    if (num === 0 || num === 1) {
+      isFlag = false;
+      return String(num) + result;
+    }
+  }
+}
+
 const num = 11;
 console.log('rf(num)', rf(num));
 console.log('rf2(num)', rf2(num));
+console.log('lf(num)', lf(num));
